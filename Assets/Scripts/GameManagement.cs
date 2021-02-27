311998bc6e56f4f239aef2b7f25911da8cbcf53a
mocavaMars33 <56504267+mocavaMars33@users.noreply.github.com>
2021-02-27 16:03:34 +1100

    Hee Kim_Cactus object and restart

---
+++ Assets/Scripts/GameManagement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagement : MonoBehaviour
+{
+    public Transform platformGenerator;
+    private Vector3 PlatformStartPoint;
+
+    public PlayerMovement thePlayer;
+    private Vector3 PlayerStartPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlatformStartPoint = platformGenerator.position;
+        PlayerStartPoint = thePlayer.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+        StartCoroutine("RestartGameCo");
+    }
+
+    public IEnumerator RestartGameCo()
+    {
+        thePlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1.0f);
+        thePlayer.transform.position = PlayerStartPoint;
+        platformGenerator.position = PlatformStartPoint;
+        thePlayer.gameObject.SetActive(true);
+    }
+
+}
+
