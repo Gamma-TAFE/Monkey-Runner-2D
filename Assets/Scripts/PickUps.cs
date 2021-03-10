79da6e4b5b52ed17e548cc8995ea636dcecda9f1
Jarrod <jarrod.athanassiou@gmail.com>
2021-03-10 17:25:29 +1100

    Pickups
    Added timer for boosts

--- Assets/Scripts/PickUps.cs
+++ Assets/Scripts/PickUps.cs
@@ -6,20 +6,16 @@ public class PickUps : MonoBehaviour
 {
     public float speedBoost = 5f;
     public float jumpBoost = 3f;
+    public float boostTime = 5f;
 
     public float minIncrease = 5f;
     public float maxIncrease = 10f;
     float increase;
 
-    public float pickUpTime = 10f;
-    public float colTime;
-
-    public bool sped;
 
     void Start()
     {
         increase = Random.Range(minIncrease, maxIncrease);
-        sped = false;
     }
 
 
@@ -30,23 +26,32 @@ public class PickUps : MonoBehaviour
 
         if (gameObject.CompareTag("Speed"))
         {
-            collision.gameObject.GetComponent<PlayerMovement>().speed += speedBoost;
-            colTime = Time.time;
-            sped = true;
-            Destroy(gameObject);
+            collision.gameObject.GetComponent<PlayerMovement>().speed += speedBoost;    //Finds float "speed" from player script and adds speed boost
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;                  //Disables Sprite
+            gameObject.GetComponent<Collider2D>().enabled = false;                      //Disables Collider
+            StartCoroutine(SpeedReset());                                               //Starts timer for boost
         }
 
         if (gameObject.CompareTag("Jump"))
         {
-            collision.gameObject.GetComponent<PlayerMovement>().jumpForce += jumpBoost;
-            Destroy(gameObject);
-            colTime = Time.time;
+            collision.gameObject.GetComponent<PlayerMovement>().jumpForce += jumpBoost; //Finds float "jumpForce" from player script and adds jump boost
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;                  //Disables Sprite
+            gameObject.GetComponent<Collider2D>().enabled = false;                      //Disables Collider
+            StartCoroutine(JumpReset());                                                //Starts timer for boost
         }
 
-       /* if (Time.time == colTime + pickUpTime && sped == true)
+       IEnumerator SpeedReset()
         {
+            yield return new WaitForSeconds(boostTime);                                 //Waits for "boostTime" before stopping the boost
             collision.gameObject.GetComponent<PlayerMovement>().speed -= speedBoost;
-            sped = false;
-        }*/
+            Destroy(gameObject);
+        }
+
+        IEnumerator JumpReset()
+        {
+            yield return new WaitForSeconds(boostTime);
+            collision.gameObject.GetComponent<PlayerMovement>().jumpForce -= jumpBoost;
+            Destroy(gameObject);
+        }
     }
 }
