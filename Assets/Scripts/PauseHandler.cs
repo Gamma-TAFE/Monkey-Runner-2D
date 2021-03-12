fe1ae35acf9963a5af6c33b4e0f2a862bd04d4f7
mocavaMars33 <56504267+mocavaMars33@users.noreply.github.com>
2021-03-12 16:06:44 +1100

    pausemenu1

--- Assets/Scripts/PauseHandler.cs
+++ Assets/Scripts/PauseHandler.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class PauseHandler : MonoBehaviour
 {
-    public static bool 
+    public static bool isPaused;
+    public GameObject pauseMenu, optionsMenu;
+
+    void Paused()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void UnPaused()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
 }
